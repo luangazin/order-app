247b5ac3dd0d53019b47c06c7eb0313fd0590d5c
Luan Gazin <luan_gazin@yahoo.com.br>
2025-07-25 20:00:09 -0300

    config: add lombok and configure cache async and TM

--- src/main/java/br/com/gazintech/orderapp/OrderAppApplication.java
+++ src/main/java/br/com/gazintech/orderapp/OrderAppApplication.java
@@ -2,12 +2,32 @@ package br.com.gazintech.orderapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableCaching
+@EnableAsync
+@EnableTransactionManagement
 public class OrderAppApplication {
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("order-app-");
+        executor.initialize();
+        return executor;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OrderAppApplication.class, args);
     }
-
 }
