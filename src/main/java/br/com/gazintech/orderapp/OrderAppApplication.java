b6c24a50735a5f332a8c29dc39a1c7137a409bdc
Luan Gazin <luan_gazin@yahoo.com.br>
2025-07-28 16:14:21 -0300

    test: creating a partner on startup application

--- src/main/java/br/com/gazintech/orderapp/OrderAppApplication.java
+++ src/main/java/br/com/gazintech/orderapp/OrderAppApplication.java
@@ -1,14 +1,18 @@
 package br.com.gazintech.orderapp;
 
+import br.com.gazintech.orderapp.entity.Partner;
+import br.com.gazintech.orderapp.repository.PartnerRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.math.BigDecimal;
 import java.util.concurrent.Executor;
 
 @SpringBootApplication
@@ -16,6 +20,9 @@ import java.util.concurrent.Executor;
 @EnableAsync
 @EnableTransactionManagement
 public class OrderAppApplication {
+
+    @Autowired
+    private PartnerRepository partnerRepository;
 
     @Bean
     public Executor taskExecutor() {
@@ -26,6 +33,21 @@ public class OrderAppApplication {
         executor.setThreadNamePrefix("order-app-");
         executor.initialize();
         return executor;
+    }
+
+    @PostConstruct
+    public void init() {
+        String code = "1ffe19fd-cb50-4afe-b4cf-07aa691631df";
+        if (!partnerRepository.existsByCode(code)) {
+            partnerRepository.save(Partner.builder()
+                    .active(true)
+                    .code(code)
+                    .name("Partener 001")
+                    .email("email")
+                    .availableCredit(BigDecimal.valueOf(10000))
+                    .creditLimit(BigDecimal.valueOf(1000))
+                    .build());
+        }
     }
 
     public static void main(String[] args) {
